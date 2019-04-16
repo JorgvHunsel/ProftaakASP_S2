075ced73f7ef1c51ef8f0933524f3c21abddfae8
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2019-04-16 09:28:05 +0200

    Added login page

---
+++ ProftaakASP_S2/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProftaakASP_S2.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+       
+    }
+}
