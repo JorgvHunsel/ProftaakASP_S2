d881a3db67c345d89bcd9dae648d7f7e3d7a1db7
Jesse Oosterwijk <I398785@fhict.nl>
2019-04-23 09:53:36 +0200

    Added UnitTests

---
+++ UnitTests/CategoryTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CategoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
