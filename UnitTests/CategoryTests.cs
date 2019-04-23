a09fe1750bee1687f6337ebcb4b289331f14f8f7
Jesse Oosterwijk <I398785@fhict.nl>
2019-04-23 11:19:59 +0200

    added Unit Tests for categories

--- UnitTests/CategoryTests.cs
+++ UnitTests/CategoryTests.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Extras.Moq;
+using Data.Contexts;
+using Data.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
 
 namespace UnitTests
 {
@@ -6,8 +12,40 @@ namespace UnitTests
     public class CategoryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetCategories_IsValid()
         {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ICategoryContext>()
+                    .Setup(x => x.GetAllCategories())
+                    .Returns(GetSampleCategory());
+
+                var cls = mock.Create<CategoryContextSQL>();
+                var expected = GetSampleCategory();
+
+                var actual = cls.GetAllCategories();
+
+                Assert.IsTrue(actual != null);
+                Assert.AreEqual(expected.Count, actual.Count);
+
+            }
+        }
+
+        private List<Category> GetSampleCategory()
+        {
+            List<Category> output = new List<Category>
+            {
+                new Category
+                {
+                    Name = "Medisch"
+                },
+                new Category
+                {
+                    Name = "Huishoudelijk"
+                }
+            };
+
+            return output;
         }
     }
 }
