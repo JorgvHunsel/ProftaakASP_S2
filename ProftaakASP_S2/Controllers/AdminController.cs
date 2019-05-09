4636a59a8832feffad27f7eff619f282abb906b8
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2019-05-09 11:30:49 +0200

    Added block user

--- ProftaakASP_S2/Controllers/AdminController.cs
+++ ProftaakASP_S2/Controllers/AdminController.cs
@@ -48,9 +48,35 @@ namespace ProftaakASP_S2.Controllers
             return View();
         }
 
+
+        [HttpGet]
         public ActionResult UserOverview()
         {
-            return View();
+            List<UserViewModel> userViewList = new List<UserViewModel>();
+            foreach (User user in _userLogic.GetAllUsers())
+            {
+                userViewList.Add(new UserViewModel(user));
+            }
+
+            return View(userViewList);
+        }
+
+        public ActionResult BlockUser(int id)
+        {
+            User updatedUser = _userLogic.GetUserById(id);
+
+            if (updatedUser.Status)
+            {
+                updatedUser.Status = false;
+            }
+            else
+            {
+                updatedUser.Status = true;
+            }
+
+            _userLogic.EditUser(updatedUser, "");
+
+            return RedirectToAction("UserOverview");
         }
     }
 }
