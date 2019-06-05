f7de6c540bdf758333e00a753dcb487044dd88f3
Jesse Oosterwijk <I398785@fhict.nl>
2019-06-05 16:47:30 +0200

    commit

---
+++ Models/ReviewInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class ReviewInfo
+    {
+        public int ReviewId { get; set; }
+        public int VolunteerId { get; set; }
+        public int CareRecipientId { get; set; }
+        public string Review { get; set; }
+        public int StarAmount { get; set; }
+
+        public ReviewInfo(int reviewId, int volunteerId, int careRecipientId, string review, int starAmmount)
+        {
+            ReviewId = reviewId;
+            VolunteerId = volunteerId;
+            CareRecipientId = careRecipientId;
+            Review = review;
+            StarAmount = starAmmount;
+        }
+    }
+}
