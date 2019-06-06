37058b6e177c17f19ed77b3801b6e4b8914117eb
WesleyMartens <44528971+WesleyMartens@users.noreply.github.com>
2019-06-06 14:04:22 +0200

    LogTests

---
+++ Data/Contexts/LogContextMock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Data.Interfaces;
+using Models;
+
+namespace Data.Contexts
+{
+    public class LogContextMock : ILogContext
+    {
+
+        public List<Log> logList = new List<Log>();
+
+        public void CreateUserLog(Log log)
+        {
+            logList.Add(log);
+        }
+
+        public List<Log> GetAllLogs()
+        {
+            return logList;
+        }
+    }
+}
